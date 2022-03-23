010a7edd5363f9fda954cd2feb12e5cebdd87ca9
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-03-23 06:06:47 +0300

    To do first completed

--- OOP/src/Lab3/Test.java
+++ OOP/src/Lab3/Test.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 public class Test
 {
+
+
     public static void main(String[] args)
     {
         Patient[] patientlist = new Patient[5];
         Patient patient = new Patient();
 
-        Patient patine1 = new Patient(001,"Patient 1",18,"Cancer");
-        Patient patine2 = new Patient(002,"Patient 2",28,"Cancer 2");
-        Patient patine3 = new Patient(003,"Patient 3",48,"Cancer 3");
-        Patient patine4 = new Patient(004,"Patient 4",38,"Cancer 4");
-        Patient patine5 = new Patient(005,"Patient 5",20,"Cancer 5");
+
+        Patient patint1 = new Patient(001,"Patient 1",18,"Cancer");
+        Patient patint2 = new Patient(002,"Patient 2",28,"Cancer 2");
+        Patient patint3 = new Patient(003,"Patient 3",48,"Cancer 3");
+        Patient patint4 = new Patient(004,"Patient 4",38,"Cancer 4");
+        Patient patint5 = new Patient(005,"Patient 5",20,"Cancer 5");
 
 
         Scanner sc=new Scanner(System.in);
@@ -35,11 +38,12 @@ public class Test
                     }
                     else
                     {
-                        patientlist[0] = patine1;
-                        patientlist[1] = patine2;
-                        patientlist[2] = patine3;
-                        patientlist[3] = patine4;
-                        patientlist[4] = patine5;
+                         storePatient(patientlist, patint1,0);
+                        storePatient(patientlist, patint2,1);
+                        storePatient(patientlist, patint3,2);
+                        storePatient(patientlist, patint4,3);
+                        storePatient(patientlist, patint5,4);
+
                         System.out.println("------------------------------------------------");
                         System.out.println("---->>>> [PPOGRAM STATUS] Patients are added succesfully");
                         System.out.println("------------------------------------------------");
@@ -65,10 +69,6 @@ public class Test
         }
 
 
-
-
-
-
     }
 
     public static void displayMenu()
@@ -78,5 +78,16 @@ public class Test
         System.out.println("***   Press 2 to print information       ***");
         System.out.println("***   Press 3 to terminate the program   ***");
     }
+
+    public static void storePatient(Patient arraylist[], Patient patientReference, int index)
+    {
+        Patient[] arraylist1 = arraylist;
+
+        arraylist1[index] = patientReference;
+
+
+    }
+
+    public 
 }
 
