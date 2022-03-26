29ca08820a84426163601ac9d2811782dd1ffb77
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-03-26 07:13:16 +0300

    Lab6 Intialized

--- OOP/src/HotelManagementSystem/Main.java
+++ OOP/src/HotelManagementSystem/Main.java
@@ -2,5 +2,19 @@ package HotelManagementSystem;
 
 public class Main
 {
-    
+    public static void main(String[] args)
+
+    {
+        System.out.println(">>>>>[PROGRAM STATUS]: Hotel Management System is running.....");
+        System.out.println("Welcome to the ECO-Hotel Management System");
+        System.out.println(">>>>>>>>Main Menu Options Page <<<<<<<<<<");
+        System.out.println("MENU OPTIONS [1 through 3] : ");
+        System.out.println("***       Press 1 for [ROOM BOOKING] page        ***");
+        System.out.println("***       Press 2 for [STAFF MANAGEMENT] page    ***");
+        System.out.println("***       Press 3 for [ROOM MANAGEMENT] page     ***");
+        System.out.println("***       Press 4 for [BILLING] page             ***");
+        System.out.println("***       Press 5 to terminate the program       ***");
+        System.out.println("Your chose?  :");
+
+    }
 }
---
+++ OOP/src/Lab6/Student.java
@@ -0,0 +1,16 @@
+package Lab6;
+
+public class Student {
+}
+class Lecture
+{
+
+}
+class Test
+{
+    public static void main(String[] args)
+    {
+
+        
+    }
+}
