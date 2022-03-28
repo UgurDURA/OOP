74dd2a7a20d9474cb96010899d923c5d88c8293a
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-03-28 22:52:54 +0300

    lazySingletion implementation
    with volatile

--- OOP/src/SingletonPattern/Client.java
+++ OOP/src/SingletonPattern/Client.java
@@ -6,7 +6,11 @@ public class Client
         EagerRegistry registry = EagerRegistry.getInstance();
         EagerRegistry registry1 = EagerRegistry.getInstance();
 
+        LazyRegistryWithDCL lazysingleton1 = LazyRegistryWithDCL.getInstance();
+        LazyRegistryWithDCL lazysingleton2 = LazyRegistryWithDCL.getInstance();
+
         System.out.println(registry==registry1);
+        System.out.println(lazysingleton1==lazysingleton2);
 
 
     }
--- OOP/src/SingletonPattern/LazyRegistryWithDCL.java
+++ OOP/src/SingletonPattern/LazyRegistryWithDCL.java
@@ -2,5 +2,41 @@ package SingletonPattern;
 
 public class LazyRegistryWithDCL
 {
-    
+    private LazyRegistryWithDCL()
+    {
+
+    }
+
+    private static volatile LazyRegistryWithDCL INSTANCE;
+
+    public static  LazyRegistryWithDCL getInstance()
+    {
+        if(INSTANCE==null)
+        {
+            synchronized (LazyRegistryWithDCL.class)
+            {
+                if(INSTANCE==null)
+                {
+                    INSTANCE = new LazyRegistryWithDCL();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
+public class Client
+{
+    public static void main(String[] args) {
+        EagerRegistry registry = EagerRegistry.getInstance();
+        EagerRegistry registry1 = EagerRegistry.getInstance();
+
+        LazyRegistryWithDCL lazysingleton1 = LazyRegistryWithDCL.getInstance();
+        LazyRegistryWithDCL lazysingleton2 = LazyRegistryWithDCL.getInstance();
+
+        System.out.println(registry==registry1);
+        System.out.println(lazysingleton1==lazysingleton2);
+
+
+    }
 }
