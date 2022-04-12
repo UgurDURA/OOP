83e569ace02e0c71b65d3a47eebbda79137da77a
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-04-12 09:29:51 +0300

    runnable implemented

--- OOP/src/CommandPattern/EWSService.java
+++ OOP/src/CommandPattern/EWSService.java
@@ -2,6 +2,9 @@ package CommandPattern;
 
 //Receiver
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class EWSService
 {
     public void addMember (String contact, String concactGroup)
@@ -46,4 +49,81 @@ class AddMemberCommand implements Command
         receiver.addMember(emailAddress, listName);
 
     }
+}
+
+//Invoker which executes commands
+//starts a worker thread in charge of executing commands
+
+class MailTaskRunner implements Runnable
+{
+    public Thread runner;
+    private List<Command> pendingCommands;
+    private volatile boolean stop;
+    private static final MailTaskRunner RUNNER = new MailTaskRunner();
+
+    public static final MailTaskRunner getInstance()
+    {
+        return RUNNER;
+    }
+
+    private MailTaskRunner()
+    {
+        pendingCommands = new LinkedList<>();
+        runner = new Thread(this);
+        runner.start();
+    }
+    @Override
+    public void run()
+    {
+         while (true)
+         {
+             Command cmd = null;
+             synchronized (pendingCommands)
+             {
+                 if (pendingCommands.isEmpty())
+                 {
+                     try
+                     {
+                         pendingCommands.wait();
+
+                     } catch (Exception e) {
+                         System.out.println("Runner interrupted");
+                         if (stop)
+                         {
+                             System.out.println("Runner Stoping");
+                             return;
+                         }
+                     }
+
+                 }
+                 else
+                 {
+                     cmd = pendingCommands.remove(0);
+                 }
+             }
+             if (cmd == null)
+             {
+                 return;
+             }
+             cmd.execute();
+         }
+
+    }
+
+    public void addCommand (Command cmd)
+    {
+        synchronized (pendingCommands)
+        {
+            pendingCommands.add(cmd);
+            pendingCommands.notifyAll();
+        }
+    }
+
+    public void shutdown()
+    {
+        this.stop = true;
+        this.runner.interrupt();
+    }
+
+
 }
