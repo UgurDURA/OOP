d4b322079beee02fa0cc3e4526176d1dff025ef2
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-04-12 09:37:34 +0300

    commandPattern[DONE]

--- OOP/src/CommandPattern/EWSService.java
+++ OOP/src/CommandPattern/EWSService.java
@@ -127,3 +127,20 @@ class MailTaskRunner implements Runnable
 
 
 }
+
+class Client
+{
+    public static void main(String[] args) throws InterruptedException {
+        EWSService service = new EWSService();
+
+        Command c1 = new AddMemberCommand("a@a.com", "spam",service);
+        MailTaskRunner.getInstance().addCommand(c1);
+
+        Command c2 = new AddMemberCommand("b@b.com","spam",service);
+        MailTaskRunner.getInstance().addCommand(c2);
+
+        Thread.sleep(3000);
+        MailTaskRunner.getInstance().shutdown();
+
+    }
+}
